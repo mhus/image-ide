2b10da89d4d0d9eed74de332a00052326c5af089
Mike Hummel <mh@mhus.de>
2022-01-09 20:00:50 +0100

    add java conductor, add ide-web

--- create.sh
+++ create.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+echo ">>> summer-ide"
 cd summer-ide
 docker build -t summer-ide . || exit 1
 cd ..
+echo ">>> summer-eclipse"
 cd summer-eclipse
 docker build -t summer-eclipse . || exit 1
 cd ..
+echo ">>> summer-ide-java"
 cd summer-ide-java
 docker build -t summer-ide-java . || exit 1
+cd ..
+echo ">>> summer-ide-web"
+cd summer-ide-web
+docker build -t summer-ide-web . || exit 1
 cd ..
