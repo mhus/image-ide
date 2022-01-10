f078ffa7273de7cd0af83ad9bd7948a9dc5a09a0
Mike Hummel <mh@mhus.de>
2022-01-10 22:23:35 +0100

    use vscode base now

--- create.sh
+++ create.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
-echo ">>> summer-ide"
+. docker.parameters
+version=${version:-latest}
+echo ">>> x11vnc-desktop:$version"
+cd ../x11vnc-desktop
+docker build -t x11vnc/desktop:$version . || exit 1
+cd -
+#echo ">>> docker-desktop:$version"
+#cd ../docker-desktop
+#docker build -t x11vnc/docker-desktop:$version . || exit 1
+#cd -
+echo ">>> vscode-desktop:$version"
+cd ../vscode-desktop
+docker build -t x11vnc/vscode-desktop:$version . || exit 1
+cd -
+echo ">>> summer-ide:$version"
 cd summer-ide
-docker build -t summer-ide . || exit 1
+docker build -t summer-ide:$version . || exit 1
 cd ..
-echo ">>> summer-eclipse"
+echo ">>> summer-eclipse:$version"
 cd summer-eclipse
-docker build -t summer-eclipse . || exit 1
+docker build -t summer-eclipse:$version . || exit 1
 cd ..
-echo ">>> summer-ide-java"
+echo ">>> summer-ide-java:$version"
 cd summer-ide-java
-docker build -t summer-ide-java . || exit 1
+docker build -t summer-ide-java:$version . || exit 1
 cd ..
-echo ">>> summer-ide-web"
+echo ">>> summer-ide-web:$version"
 cd summer-ide-web
-docker build -t summer-ide-web . || exit 1
+docker build -t summer-ide-web:$version . || exit 1
 cd ..
